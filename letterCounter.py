c4206d816bc8bfae7f7942e0bb85093590d424fb
MASKY-33 <masky.somebody33@gmail.com>
2026-06-06 20:19:34 +0200

    Create letterCounter.py

---
+++ letterCounter.py
@@ -0,0 +1,3 @@
+take_name = input("Put in name: ")
+
+print(f"Your name has {len(take_name.replace(' ', ''))} letters 🙂")  #Replace(' ', '') replaces "Programming Python" with "ProgrammingPython" for not counting the empty space
